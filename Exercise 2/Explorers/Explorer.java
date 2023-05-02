775a7c3550514d1614a855214b02d1960ee63f8d
ericasss <122081666+ericasss@users.noreply.github.com>
2023-05-02 11:46:22 +0800

    Add files via upload

---
+++ Exercise 2/Explorers/Explorer.java
@@ -0,0 +1,21 @@
+package Explorer;
+
+import Planets.Mars;
+import Planets.Planet;
+import Planets.Mercury;
+import Planets.Saturn;
+
+public interface Explorer {
+
+    void visit(Mercury mercury);
+
+    void visit(Mars mars);
+
+    void visit(Saturn saturn);
+
+    default void visit(Planet planet) {
+        System.out.println("Landed on an unknown planet...");
+    }
+} {
+    
+}
