c0cab175c6fb7f3d8c0ac70f07f79936a2b7d24a
ericasss <122081666+ericasss@users.noreply.github.com>
2023-05-30 09:34:38 +0800

    Create Pomeranian.java

---
+++ Exercise3/Dog/Pomeranian.java
@@ -0,0 +1,7 @@
+interface Pomeranian {
+
+    String characteristic = "bold, alert, intelligent, and its personality is much bigger than its size.";
+    String food = "Small Bites Chicken";
+    
+    void bark();
+}
