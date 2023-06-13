adb1a7b9ee30e31359f32bb96f0c123d77b49400
ericasss <122081666+ericasss@users.noreply.github.com>
2023-06-13 10:08:49 +0800

    Add files via upload

---
+++ Exercise 5/model/NorthPark.java
@@ -0,0 +1,5 @@
+package model;
+
+public class NorthPark extends Restaurant {
+    
+}
---
+++ Exercise 5/model/Restaurant.java
@@ -0,0 +1,23 @@
+package model;
+
+class Restaurant {
+
+    private String rate;
+    private String bestseller;
+
+    public String getRate() {
+        return this.rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getBestSeller() {
+        return this.bestseller;
+    }
+
+    public void setBestSeller(String bestseller) {
+        this.bestseller = bestseller;
+    }
+}
